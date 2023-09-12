af00ebf714c35fb4664b656933d693ace7673595
Daniel <dwsuarez98@gmail.com>
2023-09-12 16:36:20 -0300

    view: gruvbox a medias

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import requests
 
 class APIClientModel:
@@ -26,19 +27,41 @@ class APIClientView:
     def __init__(self, root):
         self.root = root
         self.root.title("POSTMateRest")
+        self.root.configure(bg="#458588")
 
-        self.url_label = tk.Label(root, text="URL:")
+        tab_control = ttk.Notebook(root)
+        style = ttk.Style()
+        style.configure("Tab.TFrame", background="#3c3836", foreground="red")
+
+        tab_get = ttk.Frame(tab_control, style="Tab.TFrame")
+        tab_post = ttk.Frame(tab_control, style="Tab.TFrame")
+        tab_get.pack_propagate(False)
+        tab_post.pack_propagate(False)
+
+        tab_control.add(tab_get, text="GET")
+        tab_control.add(tab_post, text="POST")
+        tab_control.pack(expand=1, fill="both")
+        
+        self.url_label = tk.Label(tab_get, text="URL:", bg="#3c3836", fg="#cc241d")
         self.url_label.pack()
 
-        self.url_entry = tk.Entry(root)
-        self.url_entry.pack()
+        self.url_get_entry = tk.Entry(tab_get, bg="#8ec07c")
+        self.url_get_entry.pack()
 
-        self.get_button = tk.Button(root, text="GET")
+        self.get_button = tk.Button(tab_get, text="GET")
         self.get_button.pack()
 
-        self.post_button = tk.Button(root, text="POST")
+        # POST section
+        self.url_label = tk.Label(tab_post, text="URL:", bg="#3c3836", fg="#cc241d")
+        self.url_label.pack()
+
+        self.url_post_entry = tk.Entry(tab_post)
+        self.url_post_entry.pack()
+
+        self.post_button = tk.Button(tab_post, text="POST")
         self.post_button.pack()
 
+        # RESPONSE Section
         self.response_label = tk.Label(root, text="Response:")
         self.response_label.pack()
 
