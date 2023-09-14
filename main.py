16c2083d0466ebdc3f9e8a301b6891d61d129fd1
Daniel <dwsuarez98@gmail.com>
2023-09-13 21:14:40 -0300

    doc: documentación de la clase del controlador

--- main.py
+++ main.py
@@ -87,25 +87,49 @@ class APIClientView:
 
 class APIClientController:
     def __init__(self, model, view):
-        self.model = model
-        self.view = view
+        """
+        Maneja la solicitud POST al hacer clic en el botón correspondiente.
+
+        Obtiene la URL y los datos JSON desde la vista, realiza la solicitud POST
+        a través del modelo, actualiza la vista con la respuesta y establece un 
+        mensaje de estado.
+        """
+        self.model = model # instancia del modelo
+        self.view = view # instancia de la vista
 
         self.view.get_button.config(command=self.send_get_request)
         self.view.post_button.config(command=self.send_post_request)
 
     def send_get_request(self):
+        """
+        Maneja la solicitud GET al hacer clic en el botón correspondiente.
+
+        Obtiene la URL desde la vista, realiza la solicitud GET a través del modelo,
+        actualiza la vista con la respuesta y establece un mensaje de estado.
+        """
         url = self.view.url_get_entry.get()        
         response = self.model.send_get_request(url)
-        print(response)
+
+        # Actualizar el mensaje de estado en la vista
         self.view.mensaje.set("GET Exitoso")
+
+        # Borra el contenido actual del área respuesta y muestra
+        # la respuesta nueva
         self.view.response_text.delete("1.0", tk.END)
         self.view.response_text.insert(tk.END, response)
 
     def send_post_request(self):
+        """
+        Maneja la solicitud POST al hacer clic en el botón correspondiente.
+
+        Obtiene la URL y los datos JSON desde la vista, realiza la solicitud POST
+        a través del modelo, actualiza la vista con la respuesta y establece 
+        un mensaje de estado.
+        """
         url = self.view.url_post_entry.get()
         data = self.view.data_text.get("1.0", "end-1c")
 
-        # str convert to json
+        # Convertir la cadena JSON en un diccionario de Python
         parametros = json.loads(data)
         response = self.model.send_post_request(url, parametros)
         self.view.response_text.delete("1.0", tk.END)
